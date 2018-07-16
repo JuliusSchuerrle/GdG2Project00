8061eceaa6cfb4d3361f3ca90477f4d114a45d3d
JuliusSchuerrle <hackehackekuchen@maxap.eu>
2018-07-16 22:35:26 +0200

    first Test

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,6 +1,10 @@
-public class MyApp {
+import processing.core.PApplet;
+
+public class MyApp extends PApplet {
 
     public static void  main(String[]args){
+        PApplet.main("MyApp");
+
         System.out.println("hello");
     }
 }
